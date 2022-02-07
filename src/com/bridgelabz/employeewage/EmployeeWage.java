6414eb0fbcbcd832bf54d768375f4aafae1e88a8
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-02-07 19:51:14 +0530

    wages for month

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,25 +1,51 @@
 package com.bridgelabz.employeewage;
 public class EmployeeWage {
+    public static int WORKING_DAYS = 20;
     public static void main(String[] args) {
-            Attendence attendence = new Attendence();
-            attendence.empCheck();
+        for (int i = 1;  i<= WORKING_DAYS; i++) {
+            System.out.println("For DAY"+i);
+            EmpCalculation emp = new EmpCalculation();
+            emp.empCheck();
+            DailyWage wage = new DailyWage();
+            wage.dailyWageCalculation();
+            wage.totalWage();
         }
+        System.out.println("Total Wage of employee in a month = "+DailyWage.totalWage);
     }
-    class Attendence {
-        public static final int IS_PRESENT = 1;
-        public static final int IS_ABSENT = 0;
 
-        public void empCheck() {
-            int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-            switch (empCheck) {
-                case IS_PRESENT:
-                    System.out.println("Employee is present");
-                    break;
-                case IS_ABSENT:
-                    System.out.println("Employee is absent");
-                    break;
-                default:
-                    System.out.println("Welcome");
-            }
+}
+class EmpCalculation
+{
+    public static final int IS_FULLTIME = 1;
+    public static final int IS_PARTTIME = 2;
+    public static int empHrs;
+    public void empCheck() {
+        int empCheck = (int)Math.floor(Math.random() * 10) % 3;
+        switch(empCheck) {
+            case IS_FULLTIME:
+                System.out.println("Employee is present and worked full-time");
+                empHrs = 8;
+                break;
+            case IS_PARTTIME:
+                System.out.println("Employee is present and worked part-time");
+                empHrs = 4;
+                break;
+
+            default:
+                System.out.println("Employee is absent");
+                empHrs = 0;
         }
     }
+}
+class DailyWage{
+    public static final int WAGE_PER_HOUR = 20;
+    static int totalWage = 0;
+    int totalDailyWage;
+    public void dailyWageCalculation() {
+        totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
+        System.out.println("Daily employee wage is "+totalDailyWage);
+    }
+    public void totalWage() {
+        totalWage = totalWage + totalDailyWage;
+    }
+}
